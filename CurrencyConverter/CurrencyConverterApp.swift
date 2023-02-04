94829ce54931eb0ea30de60988fffa249c8428f5
Muhammad Asad Baidar <asadbaidar@gmail.com>
2023-02-04 22:50:06 +0500

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Asad Baidar on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
